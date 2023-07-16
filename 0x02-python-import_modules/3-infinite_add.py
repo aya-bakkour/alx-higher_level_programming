bd8282bc09a2a895d84894817a66f1a87ee46a4b
aya bakkour <aya.bekour21gmail.com>
2023-07-15 17:16:40 -0700

    still coding

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 3-infinite_add.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
     """Print the addition of all arguments."""
@@ -8,4 +10,3 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
-
